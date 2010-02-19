fd66fef9051bcae539b64014f26e26bd65f91592
Shawn Balestracci <shawn@vegantech.com>
2010-02-19 05:24:26 -0800

    updated to ignore moonshine manifests

--- lib/coveralls.rb
+++ lib/coveralls.rb
@@ -1,9 +1,9 @@
 class Coveralls
   # Method to require all ruby classes when calculating code coverage.
   # Call this to not leave untested files out of the code coverage percentages.
-  def self.require_all_ruby_files(target_dirs=["/lib", "/app"])
+  def self.require_all_ruby_files(target_dirs=["lib", "app"])
     Array(target_dirs).collect do |target_dir|
-      dir = "#{RAILS_ROOT}#{target_dir}/**/*.rb"
+      dir = File.join(RAILS_ROOT, target_dir, "**","*.rb")
       Dir.glob(dir).each do |ruby_file|
         obj = ruby_file.split("#{target_dir}/",2).last.split(".rb").first 
         obj = obj.split("/",2).last if target_dir == "/app"
@@ -15,7 +15,8 @@ class Coveralls
             obj.classify.constantize
           end
         rescue NameError, LoadError
-          require obj
+          #app/manifests are used with moonshine, but not needed during project execution
+          require obj unless ruby_file.include?('app/manifests')
         end
       end
     end
